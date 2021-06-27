80200d699446c25c78a6302790f491a634333543
Jonnyb83 <64705009+Jonnyb83@users.noreply.github.com>
2021-06-27 17:21:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
-function myFunction() {
-  var x = document.getElementById("video-angelfish").autoplay;
-  document.getElementById("demo").innerHTML = x;
-}
+
+
+// Deze werkt niet
+// function myFunction() {
+//   var x = document.getElementById("fish").autoplay;
+//   document.getElementById("demo").innerHTML = x;
+// }
 
 var i = 0; 			// Start Point
 var images = [];	// Images Array
@@ -34,16 +37,82 @@ function changeImg(){
 // Run function when page loads
 window.onload=changeImg;
 
- 
+// no working
+// var vid = document.getElementById("myVideo");
+// vid.autoplay = true;
+// vid.onload();
 
 
+// something else
+// 2. This code loads the IFrame Player API code asynchronously.
+      var tag = document.createElement('script');
 
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
 
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
 
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
 
+// Get the modal
+var modal = document.getElementById("modal1");
 
+// Get the button that opens the modal
+var btn = document.getElementById("btn1");
 
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
